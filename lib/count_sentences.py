c915c57a3d16f237f72d1e6f9d6bff226ef45df9
Felleria <tellytabbie@gmail.com>
2024-06-05 12:42:47 +0300

    Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
+
+  def __init__(self, value = ""):
+    self._value = value
     
+  def get_value(self):
+    return self._value
 
-    def __init__(self, value=''):
-        self.value = value
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
 
-    @property
-    def value(self):
-        return self._value
+  value = property(get_value, set_value)
 
-    @value.setter
-    def value(self, val):
-        if not isinstance(val, str):
-            raise TypeError("Value must be a string")
-        self._value = val
+  def is_sentence(self):
+    return self._value.endswith(".")
 
-    def is_sentence(self):
-        return self.value.endswith('.')
+  def is_question(self):
+    return self._value.endswith("?")
 
-    def is_question(self):
-        return self.value.endswith('?')
+  def is_exclamation(self):
+    return self._value.endswith("!")
 
-    def is_exclamation(self):
-        return self.value.endswith('!')
-
-    def count_sentences(self):
-        import re
-        # Replace multiple punctuation marks with a single period
-        text = re.sub(r'[.!?]+', '.', self.value)
-        # Split the text by period and count non-empty parts
-        sentences = [sentence for sentence in text.split('.') if sentence.strip()]
-        return len(sentences)
+  def count_sentences(self):
+    value = self.value
+    for punc in ['!','?']:
+        value = value.replace(punc, '.')
+    
+    sentences = [s for s in value.split('.') if s]
+    
+    return len(sentences)
+  
 
 string = MyString("This is a string! It has three sentences. Right?")
-print(string.count_sentences()) 
+print(string.count_sentences())
 
